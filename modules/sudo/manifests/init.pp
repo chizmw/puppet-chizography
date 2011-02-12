b215dc2ae820b3828114a72efd65ce1bd9559ef0
Chisel <chisel@chizography.net>
2011-02-12 02:21:08 +0000

    Manage sudoers; env_keep SSH_AUTH_SOCK

--- modules/sudo/manifests/init.pp
+++ modules/sudo/manifests/init.pp
@@ -1,10 +1,12 @@
 class sudo {
     package { sudo: ensure => latest }
 
-    file { "/etc/sudoers":
-        owner => "root",
-        group => "root",
-        mode  => 440,
+    file { "etc_sudoers":
+	name	=> "/etc/sudoers",
+        owner   => "root",
+        group   => "root",
+        mode    => 440,
+        source  => "puppet:///sudo/sudoers",
         require => Package["sudo"],
     }
 }
