70b664adb974427b141641826d22b4e06a737110
Chisel <chisel@chizography.net>
2011-02-12 16:56:40 +0000

    Remove non-essential double-quotes

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,12 +1,12 @@
 node basenode {
     include concat::setup
 
-    include "build-essential"
-    include "curl"
-    include "git::client"
-    include "ssh::all"
-    include "sudo"
-    include "vim"
+    include build-essential
+    include curl
+    include git::client
+    include ssh::all
+    include sudo
+    include vim
 }
 
 node "default" inherits basenode { }
