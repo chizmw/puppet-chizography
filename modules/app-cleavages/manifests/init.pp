d11119080144489bc259b10f93a21a588f80f97b
Chisel <chisel@chizography.net>
2011-04-04 19:08:40 +0100

    Add ::1/128 access for cleavages

--- modules/app-cleavages/manifests/init.pp
+++ modules/app-cleavages/manifests/init.pp
@@ -17,10 +17,19 @@ class app-cleavages::db {
         method   => 'trust',
         pgver    => '8.4',
     }  
-    postgresql::hba { "access: db=cleavages user=cleavages type=host":
+    postgresql::hba { "access: db=cleavages user=cleavages type=host=127.0.0.1/32":
         ensure   => present,
         type     => 'host',
         address  => '127.0.0.1/32',
+        database => 'cleavages',
+        user     => 'cleavages',
+        method   => 'trust',
+        pgver    => '8.4',
+    }  
+    postgresql::hba { "access: db=cleavages user=cleavages type=host=::1/128":
+        ensure   => present,
+        type     => 'host',
+        address  => '::1/128',
         database => 'cleavages',
         user     => 'cleavages',
         method   => 'trust',
