c380d33bbdc5e57a5b95e62309def5a9f6d717c1
Chisel <chisel@chizography.net>
2011-06-27 21:42:05 +0100

    Update parley's db configuration

--- modules/app-parley/manifests/init.pp
+++ modules/app-parley/manifests/init.pp
@@ -8,9 +8,27 @@ class app-parley::db {
     postgresql::user { "parley": password => 'chitchat' }
     postgresql::database { "parley": owner => "parley" }
 
-    postgresql::hba { "access to database parley":
+    postgresql::hba { "access: db=parley user=parley type=local":
         ensure   => present,
         type     => 'local',
+        database => 'parley',
+        user     => 'parley',
+        method   => 'trust',
+        pgver    => '8.4',
+    }  
+    postgresql::hba { "access: db=parley user=parley type=host=127.0.0.1/32":
+        ensure   => present,
+        type     => 'host',
+        address  => '127.0.0.1/32',
+        database => 'parley',
+        user     => 'parley',
+        method   => 'trust',
+        pgver    => '8.4',
+    }  
+    postgresql::hba { "access: db=parley user=parley type=host=::1/128":
+        ensure   => present,
+        type     => 'host',
+        address  => '::1/128',
         database => 'parley',
         user     => 'parley',
         method   => 'trust',
