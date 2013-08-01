a00c35a23a1eee516232374d5f23ac0f06d3c25d
Chisel <chisel@chizography.net>
2013-08-01 11:37:54 +0000

    Make puppet control the nginx config

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,6 +1,14 @@
 class nginx {
     package { nginx: ensure => latest }
 
+    # we need to make the site config 'available'
+    file { "/etc/nginx/nginx.conf":
+        source => [
+            "puppet:///modules/nginx/nginx.conf"
+        ],
+        notify => Service["nginx"],
+    }
+
     service { "nginx":
         ensure  => "running",
         enable  => "true",
